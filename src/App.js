44fd18251ba1b1ec95eeed9d5f42104fa12fd4b8
viveksingh-01 <viveksingh9301@gmail.com>
2020-05-17 15:50:53 +0530

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Contact from './components/Contact';
 import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
   render() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Header(props) {
+  const { branding } = props;
   return (
-    <div>
-      <h1>{props.branding}</h1>
-    </div>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-2">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          {branding}
+        </a>
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <a href="/" className="nav-link">
+              Home
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
