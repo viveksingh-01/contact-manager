49bb6bb83dad8eb136e1a03758b2283e2b1509b8
viveksingh-01 <viveksingh9301@gmail.com>
2020-05-30 15:31:49 +0530

    Implement routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import ContactList from './components/ContactList';
+import About from './components/About';
+import AddContact from './components/AddContact';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Header branding="Contact Manager" />
-        <div className="container">
-          <ContactList />
-        </div>
+        <Router>
+          <Header branding="Contact Manager" />
+          <div className="container">
+            <Switch>
+              <Route path="/add" component={AddContact} />
+              <Route path="/about" component={About} />
+              <Route path="/" exact={true} component={ContactList} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
 
 function Header(props) {
   const { branding } = props;
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-2">
       <div className="container">
-        <a href="/" className="navbar-brand">
+        <NavLink to="/" className="navbar-brand">
           {branding}
-        </a>
+        </NavLink>
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
-            <a href="/" className="nav-link">
+            <NavLink to="/" exact={true} className="nav-link">
               Home
-            </a>
+            </NavLink>
           </li>
           <li className="nav-item">
-            <a href="/" className="nav-link">
+            <NavLink to="/add" className="nav-link">
+              Add
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/about" className="nav-link">
               About
-            </a>
+            </NavLink>
           </li>
         </ul>
       </div>
